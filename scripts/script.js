09b125056277b9adfc6cd37035dc918abb9b7137
amsteffensen <andreassteffensen@Andreass-MacBook-Air.local>
2018-10-05 22:01:52 +0200

    added card spesific js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,20 @@
 // refer to question 1 before development starts for scope document
 // connect to this api https://api.magicthegathering.io/v1/cards
 var API_URL = 'https://api.magicthegathering.io/v1/cards';
- 
+
 // get cards from api
 function loadCards(filterSearch) {
     fetch(API_URL)
-    // Convert the results to JSON format
-    .then(result => result.json())
-    .then((result) => {
-        createCards(result, filterSearch);
-    })
-    .catch(err => console.log(err))
-    }
-
+        // Convert the results to JSON format
+        .then(result => result.json())
+        .then((result) => {
+            createCards(result, filterSearch);
+        })
+        .catch(err => console.log(err))
+}
 
 // Create a function, that takes the JSON Object as an argument.
-function createCards(result, filterSearch){
+function createCards(result, filterSearch) {
     // get cards div element for all cards
     var htmlOutput = document.getElementById('cards');
     // create search result array
@@ -24,13 +23,21 @@ function createCards(result, filterSearch){
     var allCards = result.cards;
 
     // if search string is passed, create cards from search result array
-    if(filterSearch) {  
-    // filter through all the results, place them in a new array.  
+    if (filterSearch) {
+        // filter through all the results, place them in a new array.  
         searchResult = allCards.filter(card => card.name.toLowerCase().indexOf(filterSearch.toLowerCase()) !== -1);
+
+        // test if search string returns nothing
+        if (searchResult.length === 0) {
+            // if nothing is found, display error message
+            var displayMsg = document.createElement('div');
+            displayMsg.textContent = "No cards found.";
+            htmlOutput.appendChild(displayMsg);
+        }
 
         // create all card elements from search string
         for (i = 0; i < searchResult.length; i++) {
-            
+
             // create html elements for card
             var cardColumn = document.createElement('div');
             var cardContainer = document.createElement('div');
@@ -41,55 +48,53 @@ function createCards(result, filterSearch){
 
             // define html elements attributes
             cardColumn.className = "col-sm-4";
-            cardContainer.className = "card-container";             
+            cardContainer.className = "card-container";
             cardImage.style.width = "100%";
             cardImage.src = searchResult[i].imageUrl;
             cardName.textContent = searchResult[i].name;
             cardBtn.textContent = "View More";
             cardBtn.className = "btn btn-success";
             cardBtn.setAttribute("href", imageLink);
-     
+
             // draw html elements to document
             htmlOutput.appendChild(cardColumn);
             cardColumn.appendChild(cardContainer);
             cardContainer.appendChild(cardName);
             cardContainer.appendChild(cardImage);
             cardContainer.appendChild(cardBtn);
-      
         }
 
+    } else {
     // if page loads without search string create all cards
-    }  else {
-            for (i = 0; i < allCards.length; i++) {
-                // create html elements
-                var cardColumn = document.createElement('div');
-                var cardContainer = document.createElement('div');
-                var cardName = document.createElement('h4');
-                var cardImage = document.createElement('img');
-                var cardBtn = document.createElement("a");
-                var imageLink = "card-specific.html?id=" + allCards[i].id;
-                
-                // define html attributes
-                cardColumn.className = "col-sm-4";
-                cardContainer.className="card-container";
-                cardName.innerHTML = allCards[i].name;
-                cardImage.src = allCards[i].imageUrl;
-                cardImage.style.width = '100%';
-                cardBtn.setAttribute("href", imageLink);
-                cardBtn.innerHTML = "View More";
-                cardBtn.className = "btn btn-success"; 
-                
-                // draw html elements to document
-                htmlOutput.appendChild(cardColumn);
-                cardColumn.appendChild(cardContainer)
-                cardContainer.appendChild(cardName);
-                cardContainer.appendChild(cardImage);
-                cardContainer.appendChild(cardBtn);
+        for (i = 0; i < allCards.length; i++) {
+            // create html elements
+            var cardColumn = document.createElement('div');
+            var cardContainer = document.createElement('div');
+            var cardName = document.createElement('h4');
+            var cardImage = document.createElement('img');
+            var cardBtn = document.createElement("a");
+            var imageLink = "card-specific.html?id=" + allCards[i].id;
+
+            // define html attributes
+            cardColumn.className = "col-sm-4";
+            cardContainer.className = "card-container";
+            cardName.innerHTML = allCards[i].name;
+            cardImage.src = allCards[i].imageUrl;
+            cardImage.style.width = '100%';
+            cardBtn.setAttribute("href", imageLink);
+            cardBtn.innerHTML = "View More";
+            cardBtn.className = "btn btn-success";
+
+            // draw html elements to document
+            htmlOutput.appendChild(cardColumn);
+            cardColumn.appendChild(cardContainer)
+            cardContainer.appendChild(cardName);
+            cardContainer.appendChild(cardImage);
+            cardContainer.appendChild(cardBtn);
         }
-    // // if the app does not find any results show message
-    } 
+    }
 }
-  
+
 // when search button clicked, call card search function
 function addEventListener() {
     document.getElementById('searchButton').addEventListener('click', doCardSearch);
@@ -104,10 +109,10 @@ function doCardSearch(event) {
     var searchString = document.getElementById('search').value;
     // call load cards passing user search string
     loadCards(searchString);
-  }
+}
 
 // Make self invoking function for loading scripts
-(function() {
+(function () {
     loadCards();
     addEventListener();
-  })();
+})();
--- scripts/specific.js
+++ scripts/specific.js
@@ -5,3 +5,52 @@ function getQueryStringValue (key) {
 }
 // variable for the id
 var id = getQueryStringValue("id");
+
+var API_URL = 'https://api.magicthegathering.io/v1/cards/';
+
+// make api call, append card id to url
+    fetch(API_URL + id)
+      .then(result => result.json())
+      .then(result => {
+        if(result.card){
+            displayCard(result.card);
+        } else {
+            var errorMsg = document.createElement('div');
+            errorMsg.textContent = 'A card with this name could not be found. Please try again!';
+            document.querySelector('.container').appendChild(errorMsg);
+        }
+      })
+      .catch(err => console.log(err))
+
+function displayCard(card) {
+    console.log(card);
+    // get parent element
+    var cardDetails = document.getElementById('cardDetails');
+
+    // create card HTML 
+    var cardImage = document.getElementById('cardImage');
+    var cardName = document.createElement('h2');
+    var cardText = document.createElement('div');
+    var cardRarity = document.createElement('div');
+    var cardColor = document.createElement('div');
+
+    // generate image
+    var cardImageHolder = document.getElementById('cardImage');
+    var img = document.createElement('img');
+    img.src = card.imageUrl;
+    cardImageHolder.appendChild(img);
+    
+    // set card atributes
+    cardImage.src = cardDetails.imageUrl;
+    cardImage.style.width = "100%";
+    cardName.innerHTML = card.name;
+    cardText.innerHTML = '<b>About: </b>' + card.text;
+    cardRarity.innerHTML = '<b>Rarity: </b>' + card.rarity;
+    cardColor.innerHTML = '<b>Color: </b>' + card.colors;
+
+    // draw html elemts to document
+    cardDetails.appendChild(cardName);
+    cardDetails.appendChild(cardText);
+    cardDetails.appendChild(cardRarity);
+    cardDetails.appendChild(cardColor);
+}   
