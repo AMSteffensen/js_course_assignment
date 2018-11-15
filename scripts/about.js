5f7a9c9d397055ddcb1b3574e35ff16476bc41e8
Andreas Steffensen <andreas.steffensen89@gmail.com>
2018-11-15 19:27:28 +0100

    fixed valdidation erros in about.js

--- scripts/about.js
+++ scripts/about.js
@@ -1,38 +1,43 @@
+
+
 // refer to question 3 before development starts for scope document
 
 function replaceText() {
     // get text to replace
-    var div = document.getElementById('aboutText');
+    var div = document.getElementById("aboutText");
     // define regex text string
     var regex = /Magic/gi;
     var aboutText = div.innerHTML;
-
-    div.innerHTML = aboutText.replace(regex, 'Something');
-}
-
-function toggleDisplay() {
-    // get panel element 
-    var panel = document.getElementById('moreInfoContent');
-
+  
+    div.innerHTML = aboutText.replace(regex, "Something");
+  }
+  
+  function toggleDisplay() {
+    // get panel element
+    var panel = document.getElementById("moreInfoContent");
+  
     // check if panel is expanded
-    if(panel.style.display == "block") {
-            // collapse panel 
-            panel.style.display = "none";
+    if (panel.style.display === "block") {
+      // collapse panel
+      panel.style.display = "none";
+    } else {
+      // expand panel
+      panel.style.display = "block";
     }
-    else {
-        // expand panel
-        panel.style.display = "block";
-    }
-}  
-
-// add event listener for panel dispaly 
-function addEventListener() {
-    var moreInfoTrigger = document.getElementById('moreInfoTrigger');
-    moreInfoTrigger.addEventListener('click', toggleDisplay)
-}
-
-// Make self invoking function for loading scripts
-(function () {
+  }
+  
+  // add event listener for panel dispaly
+  function addEventListener() {
+    var moreInfoTrigger = document.getElementById("moreInfoTrigger");
+    moreInfoTrigger.addEventListener("click", toggleDisplay);
+  }
+  
+  // Make self invoking function for loading scripts
+  (function() {
+    "use strict";
     replaceText();
     addEventListener();
-})();
+  })();
+  
+  
+  
